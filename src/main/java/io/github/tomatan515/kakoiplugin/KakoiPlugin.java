f16dc44b8c3502c54d4c300e1fa0c6d3ce6f8cc3
tomatan515 <tomatan515@gmail.com>
2021-05-19 05:23:02 +0900

    first commit.

---
+++ src/main/java/io/github/tomatan515/kakoiplugin/KakoiPlugin.java
@@ -0,0 +1,17 @@
+package io.github.tomatan515.kakoiplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KakoiPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
